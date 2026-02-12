da565e7aea442f3b34dc4f8935a582f898b47769
raghab-pandit01 <raghab.pandit@w3developers.com.np>
2026-02-12 20:13:13 +0545

    feat: integrate Tailwind CSS and fetch quotes API
    - Added Tailwind CSS as a dependency and configured it in vite.config.js.
    - Updated App component to fetch quotes from the API and display them in a styled format.
    - Removed default styles and replaced them with Tailwind CSS classes for improved design.
    - Modified index.css to include Tailwind CSS and set a global background color.

--- backend/puppeteer/puppeteer.js
+++ backend/puppeteer/puppeteer.js
@@ -1,9 +1,31 @@
-const Puppeteer= require('puppeteer');
+import puppeteer from "puppeteer";
 
-(async ()=>{
-    const browser= await Puppeteer.launch({headless: false});
-    const page= await browser.newPage()
-    await page.goto('https://quotes.toscrape.com/')
+export const pupFunc= async ()=> {
+    const browser= await puppeteer.launch();
+    const page= await browser.newPage();
 
-    await page.click('nav ul li a')
-})();
+    let totalQuotes= [];
+
+    for(let i=0;i<=10;i++){
+
+        await page.goto(`https://quotes.toscrape.com/page/${i}/`)
+//    await page.screenshot({ path: 'operation.jpeg' })
+
+    let Quotes= await page.evaluate( ( ) =>{
+        const pgTag= document.querySelectorAll(".container .row .col-md-8 .quote");
+
+        const quot= [...pgTag].map(tags => ({
+            Quote: tags.querySelector('span.text')?.innerText || '',
+            Author: tags.querySelector('small.author')?.innerText || '',
+            Tag: [...tags.querySelectorAll('.tags a')].map(tag => tag.innerText)
+        }));
+        return quot;
+
+    })
+
+    totalQuotes= [...totalQuotes, ...Quotes]
+}
+
+    await browser.close()
+    return totalQuotes
+}
