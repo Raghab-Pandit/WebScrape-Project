0e5cd73b86e4e3422c42ad8030df09d1c4994a02
raghab-pandit01 <raghab.pandit@w3developers.com.np>
2026-02-17 03:29:42 +0545

    style: update header and remove unnecessary elements for cleaner UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,14 +20,8 @@ const App = () => {
 
   return (
     <>
-    <div className="w-full p-15 flex items-center justify-center space-x-2 bg-[#242424]/70 fixed">
-        <span className="text-4xl font-bold text-blue-400 hidden sm:block">
-          "
-        </span>
-      <h1 className="font-bold text-5xl text-white">Scraped News</h1>
-        <span className="text-4xl font-bold text-blue-400 hidden sm:block">
-          "
-        </span>
+    <div className="w-full p-15 flex items-center justify-start space-x-2 bg-[#242424]/70 fixed">
+      <h1 className="font-bold text-5xl text-white">NewsNet24</h1>
     </div>
     <div className="flex flex-col items-center justify-center">
         <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-x-6 gap-x-2 sm:gap-y-14 gap-y-8 mt-45">
@@ -36,15 +30,9 @@ const App = () => {
           return(
     <div key={i} className="w-70 text-center text-white font-semibold text-2xl flex flex-col justify-between border-4 border-[#99E1D9] rounded-2xl">
         <div className="p-3 space-x-2">
-        <span className="text-4xl font-bold text-yellow-400">
-          "
-        </span>
         <span>
           {newz.headline}
         </span>  
-        <span className="text-4xl font-bold text-yellow-400">
-          "
-        </span>
         </div>  
         <div className="bg-[#99E1D9] text-black">
           -{newz.source}-
