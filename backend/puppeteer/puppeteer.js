dcfc83bcfee19605edbd7be0323a32b1f99fbbae
raghab-pandit01 <raghab.pandit@w3developers.com.np>
2026-02-17 16:10:39 +0545

    feat: update news sources and enhance news item display with links

--- backend/puppeteer/puppeteer.js
+++ backend/puppeteer/puppeteer.js
@@ -3,17 +3,22 @@ import puppeteer from "puppeteer";
 export const pupFunc= async ()=> {
     const browser= await puppeteer.launch();
     const page= await browser.newPage(); 
-    const sites=[{source:'NYTimes', link:"https://www.nytimes.com/athletic/"}, {source:'BBC',link:"https://www.bbc.com/sport"}]
+    const sites=
+    [
+        {source: 'ABCNepal', link:'https://www.abcnepal.tv/posts/category/%e0%a4%96%e0%a5%87%e0%a4%b2%e0%a4%95%e0%a5%81%e0%a4%a6'}
+        ,{source: 'Kantipur', link:'https://kantipurtv.com/sports'}
+    ]
     let totalNews= [];
 
     for(const site of sites){
         let selector;
-        if(site.source==='BBC'){
-            selector= 'div:nth-child(2) div:first-child div:first-child div:nth-child(2) div:first-child main#main-content div:nth-child(4) div:first-child div:first-child div:first-child div:first-child div:first-child div:first-child div:first-child h3 a span p span'
+        if(site.source==='ABCNepal'){
+            selector= 'div.banner-section div.uk-container div:first-child div:first-child div:nth-child(3) div div:first-child div:nth-child(2) div h3 a'
         }
-        else if(site.source==='NYTimes'){
-            selector= 'div:first-child div:first-child div:nth-child(2) main div:first-child div:nth-child(3) div:first-child div:first-child div:first-child div:first-child div:nth-child(2) div:first-child div div:first-child a div:first-child span:first-child div:first-child div:first-child div:first-child p'
-         }
+        else if(site.source==='Kantipur'){
+            selector= 'div.page-wrap section div.container div.row div:first-child div#loadContent article div.row div:first-child h4 a'
+        }
+
         await page.goto(site.link)
         await page.waitForSelector(selector)
         const News= await page.evaluate((site, selector)=>{
@@ -23,6 +28,7 @@ export const pupFunc= async ()=> {
             const info =[...newsz].map((news)=>({
                 source: site.source,
                 headline: news?.innerText || '',
+                link: news?.href || ''
             }))
 
             return info
